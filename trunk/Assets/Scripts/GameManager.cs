07307479590d9e2ce46c056cad5cdfd7c1240983
pacheco@calvinpacheco.com <pacheco@calvinpacheco.com@1fe1b5a8-aa4b-3d33-b448-89f94256a209>
2013-12-16 02:20:44 +0000

    Almost Done!

--- trunk/Assets/Scripts/GameManager.cs
+++ trunk/Assets/Scripts/GameManager.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    public enum GameState {
+        Start,
+        OnGame,
+        Lose
+    }
+
     public float _environmentAceleration;
     public float _environmentSpeedSmoothing;
     public int _forwardWay = -1;
@@ -11,17 +17,35 @@ public class GameManager : MonoBehaviour {
     public static float EnvironmentSpeedSmoothing;
     public static float CameraHorizontalExtent;
     public static int ForwardWay;
-	
+
+    public static GameState CurrentGameState;
+
+    void Start() {
+        CurrentGameState = GameState.Start;
+    }
 	// Update is called once per frame
 	void Update () {
+        switch (CurrentGameState) {
+            case GameState.Start:
+                Camera.main.camera.GetComponent<CameraMoveAt>()._offsetX = 12;
+                break;
+            case GameState.OnGame:
+                Camera.main.camera.GetComponent<CameraMoveAt>()._offsetX = 4;
+                AddingMoreSpeed();
+                break;
+            case GameState.Lose:
+                Camera.main.camera.GetComponent<CameraMoveAt>()._offsetX = -4;
+                break;            
+        }
+
         CameraHorizontalExtent    = Camera.main.camera.orthographicSize * Screen.width / Screen.height;
         EnvironmentAceleration    = _environmentAceleration;
         EnvironmentSpeedSmoothing = _environmentSpeedSmoothing;
         ForwardWay                = _forwardWay;
-        if ((Input.GetKey(KeyCode.KeypadEnter)) || (Input.GetKey(KeyCode.Space))) {
-            Camera.main.camera.GetComponent<CameraMoveAt>()._offsetX = 0;
-            // Enable game
-        }
+        //if ((Input.GetKey(KeyCode.KeypadEnter)) || (Input.GetKey(KeyCode.Space))) {
+        //    GameState = GameState.OnGame;
+        //    // Enable game
+        //}
         //AddingMoreSpeed();
 	}
 
